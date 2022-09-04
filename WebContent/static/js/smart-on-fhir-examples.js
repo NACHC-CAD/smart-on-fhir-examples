c8b1ee3c4fe811de51a1d842c84ee50599a31d66
greshje <greshje@gmail.com>
2022-09-04 16:56:12 -0400

    getting patient on server from post request

--- WebContent/static/js/smart-on-fhir-examples.js
+++ WebContent/static/js/smart-on-fhir-examples.js
@@ -2,10 +2,21 @@ doGet = function(url, locator) {
 	$.ajax({
 	  url: url,
 	  success: function(data) {
-	    $(locator).html(data);
-	    alert('Load was performed.');
+		try {
+		    $(locator).html(data);
+		} 
+		catch(error) {}
 	  }
 	});
 }
 
-
+doPost = function(url,locator,reqObj) {
+	success = function(data) {
+		try {
+		    $(locator).html(data);
+		} 
+		catch(error) {}
+	}
+	reqData = JSON.stringify(reqObj);
+	$.post(url,reqData,success);
+}
--- src/main/java/org/nachc/smartonfhirexamples/action/example005/GetBasicPatientInfo.java
+++ src/main/java/org/nachc/smartonfhirexamples/action/example005/GetBasicPatientInfo.java
@@ -3,7 +3,14 @@ package org.nachc.smartonfhirexamples.action.example005;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.nachc.smartonfhirexamples.action.abs.AppAction;
+
+import com.nach.core.util.file.FileUtil;
+import com.nach.core.util.http.HttpClientFactory;
+import com.nach.core.util.http.HttpRequestClient;
+import com.nach.core.util.json.JsonUtil;
+import com.nach.core.util.string.StringUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,9 +19,20 @@ public class GetBasicPatientInfo extends AppAction {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) {
-		log.info("Doing hello world action");
-		log.info("Done");
-		this.forward(req, resp, "/WEB-INF/jsp/pages/hello/HelloWorld.jsp");
+		try {
+			log.info("Getting basic patient info from JSON string...");
+			String patientJson = IOUtils.toString(req.getReader());
+			patientJson = JsonUtil.prettyPrint(patientJson);
+			String msg = "";
+			msg += "Got patient string: \n\nSTART PATIENT ------------------------";
+			msg += patientJson;
+			msg += "\nEND PATIENT ------------------------------------------------";
+			log.info(msg);
+			this.forward(req, resp, "/WEB-INF/jsp/pages/005-serverside/impl/PatientInfo.jsp");
+			log.info("Done");
+		} catch(Exception exp) {
+			throw new RuntimeException(exp);
+		}
 	}
 
 }
