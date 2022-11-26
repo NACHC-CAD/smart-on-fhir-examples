eabcdccd324312d59ee2ac4f37e42eac7a256ccd
greshje <greshje@gmail.com>
2022-11-26 17:52:06 -0500

    work for smart-on-fhir connectathon

--- src/test/java/org/nachc/testing/external/fhir/patient/GetPatientResourcesFromTestFiles.java
+++ src/test/java/org/nachc/testing/external/fhir/patient/GetPatientResourcesFromTestFiles.java
@@ -1,10 +1,12 @@
 package org.nachc.testing.external.fhir.patient;
 
 import java.io.File;
+import java.util.List;
 
-import org.hl7.fhir.dstu3.model.Patient;
-import org.nachc.tools.fhirtoomop.fhir.patient.FhirPatient;
-import org.nachc.tools.fhirtoomop.fhir.patient.factory.FhirPatientFactory;
+import org.hl7.fhir.r4.model.DiagnosticReport;
+import org.hl7.fhir.r4.model.Patient;
+import org.nachc.tools.fhirtoomop.fhir.patient.r4.FhirPatient;
+import org.nachc.tools.fhirtoomop.fhir.patient.r4.factory.FhirPatientFactory;
 
 import com.nach.core.util.fhir.parser.FhirJsonParser;
 import com.nach.core.util.file.FileUtil;
@@ -17,29 +19,29 @@ import lombok.extern.slf4j.Slf4j;
 public class GetPatientResourcesFromTestFiles {
 
 	private static final String ROOT = "src/test/resources/fhir/patients";
-	
+
 	public static void main(String[] args) {
 		// delete existing output directories
 		log.info("Cleaning up old dirs...");
 		log.info("Removing patient...");
 		FileUtil.rmdir(new File(ROOT, "patient"));
-		log.info("Removing patient...");
-		FileUtil.rmdir(new File(ROOT, "patient"));
-		// set the fhir context to DSTU3
+		// create the patient dir
+		File root = FileUtil.getFromProjectRoot(ROOT);
+		File patientRoot = new File(root, "patient");
+		FileUtil.mkdirs(patientRoot);
+		log.info("PATIENT ROOT: " + FileUtil.getCanonicalPath(patientRoot));
+		// set the fhir context to R4
 		log.info("Setting context...");
-		FhirJsonParser.setFhirContext(FhirContext.forDstu3());
+		FhirJsonParser.setFhirContext(FhirContext.forR4());
 		// get the patients
 		log.info("Getting patient info for test patients...");
-		File root = FileUtil.getFromProjectRoot(ROOT);
 		File patientEverythingDir = new File(root, "_everything");
-		File[] dirList = patientEverythingDir.listFiles();
+		File[] fileList = patientEverythingDir.listFiles();
 		// for each patient
-		for(File dir : dirList) {
-			FhirPatient patient = FhirPatientFactory.build(dir);
-			String patientId = patient.getPatientId();
-			File patientDir = new File(root, "patient");
-			FileUtil.mkdirs(patientDir);
-			log.info("Got patient: " + patientId);
+		for (File file : fileList) {
+			FhirPatient patient = FhirPatientFactory.buildFromSingleFile(file);
+			File patientDir = FileUtil.mkdirs(patientRoot, file.getName());
+			log.info("Got patient: " + file.getName());
 			writePatient(patient, patientDir);
 		}
 		log.info("Done.");
@@ -49,11 +51,32 @@ public class GetPatientResourcesFromTestFiles {
 		Patient fhirPatient = patient.getPatient().getPatient();
 		String json = FhirJsonParser.serialize(fhirPatient);
 		json = JsonUtil.prettyPrint(json);
-		String patientId = patient.getPatientId();
-		File patientDir = FileUtil.mkdirs(new File(dir, patientId));
-		File patientFile = new File(patientDir, patientId);
+		File patientFile = new File(dir, dir.getName());
 		FileUtil.write(json, patientFile);
-		log.info("File written to: " + FileUtil.getCanonicalPath(patientFile));		
+		writeDiagnosticReports(patient, dir);
+		log.info("File written to: " + FileUtil.getCanonicalPath(patientFile));
 	}
-	
+
+	private static void writeDiagnosticReports(FhirPatient patient, File patientDir) {
+		File dir = FileUtil.mkdirs(patientDir, "DiagnosticReport");
+		List<DiagnosticReport> list = patient.getResourceListForType(DiagnosticReport.class);
+		int cnt = 0;
+		for(DiagnosticReport report : list) {
+			cnt++;
+			String json = FhirJsonParser.serialize(report);
+			json = JsonUtil.prettyPrint(json);
+			String fileName = "report-";
+			if(cnt < 10) {
+				fileName += "0";
+			}
+			if(cnt < 100) {
+				fileName += "0";
+			}
+			fileName +=  cnt + ".json";
+			File file = new File(dir, fileName);
+			FileUtil.write(json, file);
+		}
+		log.info("Got " + list.size() + " Diagnostic Reports.");
+	}
+
 }
